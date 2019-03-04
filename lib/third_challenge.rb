8ba37bfed49d211d5e3a753149e2268beb6a107b
Princeton Rose <decoyzpr@gmail.com>
2019-03-04 18:50:43 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -8,8 +8,8 @@ def third_challenge
         },
       :matriarch => {
         #your key/value pairs here
-        :name => "Lady Capulet",
-        :age => "51"
+        :name => "Lady Montague",
+        :age => "54"
         },
       :hero => {
         #your key/value pairs here
